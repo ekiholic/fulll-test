879d16ab1433e1f5428514caf921404577cb7ffd
Cedric Cheng <cheng-cedric@hotmail.fr>
2025-02-06 14:47:09 +0100

    Add .gitignore and FizzBuzz algo finished

---
+++ Algo/algo.js
@@ -0,0 +1,35 @@
+function checkRules(rules) {
+    if (typeof rules !== "object" || rules === null) {
+        return false;
+    }
+    for (const [key, value] of Object.entries(rules)) {
+        if (Number(key) == NaN || typeof value !== "string") {
+            return false
+        }
+    }
+    return true;
+}
+
+function fizzbuzz(n, rules) {
+    if (typeof n !== "number" || !checkRules(rules)) {
+        console.error("Invalid type");
+        return;
+    }
+    for (let i = 1; i <= n; i++) {
+        let output = "";
+        for (const [divisor, word] of Object.entries(rules)) {
+            if (i % divisor === 0) {
+                output += word;
+            }
+        }
+        console.log(output || i);
+    }
+}
+
+// Usage example
+const rules = {
+    3 : "Fizz",
+    5 : "Buzz",
+}
+
+fizzbuzz(20, rules)

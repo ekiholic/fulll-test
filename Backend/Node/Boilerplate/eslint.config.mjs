6dda9be6b4952ef9e36d2ba2eacd4fc33db601dd
Cedric Cheng <cheng-cedric@hotmail.fr>
2025-02-12 16:42:22 +0100

    Update eslint.config.mjs

--- Backend/Node/Boilerplate/eslint.config.mjs
+++ Backend/Node/Boilerplate/eslint.config.mjs
@@ -1,34 +1,12 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginTs from "@typescript-eslint/eslint-plugin";
-import parser from "@typescript-eslint/parser";
+import tseslint from "typescript-eslint";
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ["./src/**/*.ts", "./features/**/*.ts"],
-    languageOptions: {
-      sourceType: "module",
-    },
-    parser: parser,
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: "module",
-    },
-    env: { node: true },
-    extends: [
-      pluginTs.configs.recommended,
-      pluginJs.configs.recommended,
-    ],
-  },
-  {
-    files: ["./features/step_definitions/steps.js"],
-    languageOptions: { sourceType: "commonjs" },
-    env: { node: true },
-    globals: globals.node,
-    extends: [
-      pluginJs.configs.recommended,
-    ],
-  },
+  {files: ["./src/**/*.ts"]},
+  {languageOptions: { globals: globals.node }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
 ];
